c98e6a69fabc9aa7250e886cd80b23260c4a53d3
coding-seahorse <115553545+coding-seahorse@users.noreply.github.com>
2022-10-21 14:27:24 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector("body");
-const btn = document.querySelector(".fa-moon");
-
-btn.addEventListener('click', function(){
-    body.classList.toggle("dark-mode")
-});
